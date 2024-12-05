eb18f33bbe2b06a7c48abaa1f220cce8b3742480
A6979 <mayur.mangal@axtria.com>
2024-12-05 12:21:42 +0530

    Added code for Threadlocal and Added COde for Extent Report

--- src/main/java/com/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/opencart/factory/PlaywrightFactory.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Properties;
 
 public class PlaywrightFactory {
@@ -17,38 +19,61 @@ public class PlaywrightFactory {
 
     Properties prop;
 
+    private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+    private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+    private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+    private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+
+
+    public static Playwright getPlaywright() {
+        return tlPlaywright.get();
+    }
+
+    public static Browser getBrowser() {
+        return tlBrowser.get();
+    }
+
+    public static BrowserContext getBrowserContext() {
+        return tlBrowserContext.get();
+    }
+
+    public static Page getPage() {
+        return tlPage.get();
+    }
 
     public Page initBrowser(Properties prop) {
-        playwright = Playwright.create();
+
+        tlPlaywright.set(Playwright.create());
+//        playwright = Playwright.create();
         String browserName = prop.getProperty("browser").trim();
 
         System.out.println("Browser Name is:" + browserName);
 
         switch (browserName.toLowerCase()) {
             case "chromium":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
                 break;
 
             case "firefox":
-                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
                 break;
 
             case "safari":
-                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
                 break;
 
             case "chrome":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false)));
                 break;
 
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browserName);
         }
-
-        browserContext = browser.newContext();
-        page = browserContext.newPage();
-        page.navigate(prop.getProperty("url").trim());
-        return page;
+        tlBrowserContext.set(getBrowser().newContext());
+        tlPage.set(getBrowserContext().newPage());
+        getPage().navigate(prop.getProperty("url").trim());
+        return getPage();
     }
 
     public Properties init_prop() throws FileNotFoundException {
@@ -65,6 +90,21 @@ public class PlaywrightFactory {
         }
 
         return prop;
+    }
+
+    /**
+     * take screenshot
+     *
+     */
+
+    public static String takeScreenshot() {
+        String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+        //getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+
+        byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+        String base64Path = Base64.getEncoder().encodeToString(buffer);
+
+        return base64Path;
     }
 
 
